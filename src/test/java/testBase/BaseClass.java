075fc283ca30e787136e0ef5fe8eec16c7fd8005
Raushan Kumar <vicky.raushan@gmail.com>
2022-02-09 21:44:12 +0530

    Added BaseClass.java file

---
+++ src/test/java/testBase/BaseClass.java
@@ -0,0 +1,31 @@
+package testBase;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+public class BaseClass {
+
+    public WebDriver driver;
+    public Properties p;
+    public FileInputStream fis;
+
+    @BeforeClass
+    public void setUp() throws IOException {
+        p=new Properties();
+        fis=new FileInputStream(".//src//main//resources//config.properties");
+        p.load(fis);
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+}
